a8a278ef397f6c953684c30f87be37961ec26f8b
Akash Kumar Singh <itsakash18.06@gmail.com>
2023-08-08 11:54:22 +0530

    add blog functionality updated

--- Project 1/script.js
+++ Project 1/script.js
@@ -10,3 +10,33 @@ addBtn.addEventListener("click", () => {
 cutBtn.addEventListener("click", () => {
     addBlog.classList.remove("active")
 })
+
+
+let addImgUrl = document.getElementById("addImgUrl").value
+let validateImgUrl = "[(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)"
+if (addImgUrl === validateImgUrl) {
+
+    document.getElementById("addBlogBtn").addEventListener("click", (e) => {
+        let addImgUrl = document.getElementById("addImgUrl").value
+        let addBlogTitle = document.getElementById("addBlogTitle").value
+        let addBlogDesc = document.getElementById("addBlogDesc").value
+        let addBlogDetail = document.getElementById("addBlogDetail").innerHTML
+
+        let mainSection = document.getElementById("mainSection")
+
+        let mainContainer = document.createElement("div")
+        mainContainer.classList.add("container")
+        mainContainer.innerHTML = `<img src="${addImgUrl}" alt="">
+    <p class="blog-title">${addBlogTitle}</p>
+    <p class="blog-desc">${addBlogDesc}</p>
+    <p id="warning"></p>
+    <button class="read-btn" id="readBtn">Read</button>`
+
+        mainSection.appendChild(mainContainer)
+
+    })
+}
+else {
+    document.getElementById("warning").innerText = "Invalid Url"
+    console.log("Invalid Url")
+}
