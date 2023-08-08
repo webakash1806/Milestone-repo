0215ad47e73fea600ad91a9f4983a18c93e5442e
Akash Kumar Singh <itsakash18.06@gmail.com>
2023-08-08 12:47:28 +0530

    modal functionaity updated

--- Project 1/script.js
+++ Project 1/script.js
@@ -12,31 +12,25 @@ cutBtn.addEventListener("click", () => {
 })
 
 
-let addImgUrl = document.getElementById("addImgUrl").value
-let validateImgUrl = "[(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)"
-if (addImgUrl === validateImgUrl) {
 
-    document.getElementById("addBlogBtn").addEventListener("click", (e) => {
-        let addImgUrl = document.getElementById("addImgUrl").value
-        let addBlogTitle = document.getElementById("addBlogTitle").value
-        let addBlogDesc = document.getElementById("addBlogDesc").value
-        let addBlogDetail = document.getElementById("addBlogDetail").innerHTML
+document.getElementById("addBlogBtn").addEventListener("click", (e) => {
+    let addImgUrl = document.getElementById("addImgUrl").value
+    let addBlogTitle = document.getElementById("addBlogTitle").value
+    let addBlogDesc = document.getElementById("addBlogDesc").value
+    let addBlogDetail = document.getElementById("addBlogDetail").innerHTML
 
-        let mainSection = document.getElementById("mainSection")
+    let mainSection = document.getElementById("mainSection")
 
-        let mainContainer = document.createElement("div")
-        mainContainer.classList.add("container")
-        mainContainer.innerHTML = `<img src="${addImgUrl}" alt="">
+    let mainContainer = document.createElement("div")
+    mainContainer.classList.add("container")
+    mainContainer.innerHTML = `<img src="${addImgUrl}" alt="">
     <p class="blog-title">${addBlogTitle}</p>
     <p class="blog-desc">${addBlogDesc}</p>
-    <p id="warning"></p>
     <button class="read-btn" id="readBtn">Read</button>`
 
-        mainSection.appendChild(mainContainer)
+    mainSection.appendChild(mainContainer)
 
-    })
-}
-else {
-    document.getElementById("warning").innerText = "Invalid Url"
-    console.log("Invalid Url")
-}
+    // document.getElementById("readBtn").addEventListener("click",(e)=>{
+
+    // })
+})
