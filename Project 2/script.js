70b8cde7b7121c736d7781ecc45aa901fbf86583
Akash Kumar Singh <itsakash18.06@gmail.com>
2023-08-28 15:17:44 +0530

    Api Added

--- Project 2/script.js
+++ Project 2/script.js
@@ -1,14 +1,38 @@
-let movieUrl = "https://www.omdbapi.com/?s=action&apikey=dde1d56d"
+let searchBtn = document.getElementById("searchBtn")
 
-async function fetchData() {
-    try {
-        const response = await fetch(movieUrl)
-        const data = await response.json()
-        console.table(data.Search)
-    }
-    catch (err) {
-        console.log(err)
-    }
-}
+searchBtn.addEventListener("click", () => {
+    let movieTitle = document.getElementById("searchMovie").value
+    // console.log(movieTitle)
 
-fetchData()
+    let movieUrl = `https://www.omdbapi.com/?s=${movieTitle}&apikey=dde1d56d`
+
+    async function fetchData() {
+        try {
+            const response = await fetch(movieUrl)
+            const data = await response.json()
+            // console.table(data.Search)
+            let movieMainContainer = document.getElementById("movieMainContainer")
+
+            movieMainContainer.innerHTML = data.Search.map((movie) => {
+                return `<div class="movie-container" id="movieContainer">
+            <img src="${movie.Poster}"
+                alt="">
+            <p class="title" id="movieTitle">${movie.Title}</p>
+            <button>Watch Now</button>
+        </div>`
+            });
+
+            console.log(movieMainContainer)
+
+
+
+
+        }
+        catch (err) {
+            console.log(err)
+        }
+    }
+
+    fetchData()
+})
+
