8bb0c7ff3f261f9a97ed242a04780fb390898395
Akash Kumar Singh <itsakash18.06@gmail.com>
2023-08-28 20:16:26 +0530

    JS and Styles updated

--- Project 2/script.js
+++ Project 2/script.js
@@ -2,37 +2,42 @@ let searchBtn = document.getElementById("searchBtn")
 
 searchBtn.addEventListener("click", () => {
     let movieTitle = document.getElementById("searchMovie").value
-    // console.log(movieTitle)
-
+    let movieMainContainer = document.getElementById("movieMainContainer")
     let movieUrl = `https://www.omdbapi.com/?s=${movieTitle}&apikey=dde1d56d`
 
-    async function fetchData() {
-        try {
-            const response = await fetch(movieUrl)
-            const data = await response.json()
-            // console.table(data.Search)
-            let movieMainContainer = document.getElementById("movieMainContainer")
+    if (movieTitle == "") {
+        movieMainContainer.innerHTML = `<h1 class="error-message">OOPS! Search Box is Empty</h1>`
+    }
+    else {
+        async function fetchData() {
+            try {
+                const response = await fetch(movieUrl)
+                const data = await response.json()
+                // console.table(data.Search)
 
-            movieMainContainer.innerHTML = data.Search.map((movie) => {
-                return `<div class="movie-container" id="movieContainer">
+                let movieData = data.Search.map((movie) => {
+                    return `<div class="movie-container" id="movieContainer">
             <img src="${movie.Poster}"
                 alt="">
             <p class="title" id="movieTitle">${movie.Title}</p>
             <button>Watch Now</button>
         </div>`
-            });
+                });
 
-            console.log(movieMainContainer)
+                movieMainContainer.innerHTML = movieData.join('')
 
+                console.log(movieMainContainer)
 
-
-
+            }
+            catch (err) {
+                movieMainContainer.innerHTML = `<h1 class="error-message">OOPS! Nothing is here</h1>`
+                console.log(err)
+            }
         }
-        catch (err) {
-            console.log(err)
-        }
+        fetchData()
+
     }
 
-    fetchData()
+
 })
 
